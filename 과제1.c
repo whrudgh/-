4a6d39dfd73a00f831b144a01396581e21bd0c84
subin073 <144412727+subin073@users.noreply.github.com>
2024-03-26 11:34:13 +0900

    Update and rename 과제1.cpp to 과제1.c

--- 과제1.c
+++ 과제1.c
@@ -4,7 +4,7 @@ int main(void) {
 	char str1[] = "Korea";
 	char str2[] = "Seoul";
 	char str3[] = "Mapo";
-	char str4[] = "152¹øÁö 2/3";
+	char str4[] = "152Â¹Ã¸ÃÃ¶ 2/3";
 
 	printf("%s \n %s \n %s \n %s\n", str1, str2, str3, str4);
 
